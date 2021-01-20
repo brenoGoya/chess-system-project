55b05a7f4e1af4e5f5724abf5b0cef2f1f369a9b
Breno Goya <breno.goya@gmail.com>
2021-01-20 18:12:36 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -55,9 +55,15 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		if (chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		}
+		else {
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
 	}
 	
@@ -111,12 +117,12 @@ public class UI {
 		System.out.print("White: ");
 		System.out.print(ANSI_WHITE);
 		System.out.println(Arrays.toString(white.toArray()));		
-		System.out.println(ANSI_RESET);
+		System.out.print(ANSI_RESET);
 				
 		System.out.print("Black: ");
 		System.out.print(ANSI_YELLOW);
 		System.out.println(Arrays.toString(black.toArray()));		
-		System.out.println(ANSI_RESET);
+		System.out.print(ANSI_RESET);
 	}
 	
 }
